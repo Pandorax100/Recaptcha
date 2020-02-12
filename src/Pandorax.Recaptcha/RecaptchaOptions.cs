03143ceddabbfbe8795cfec60de43eb2221f498f
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2020-02-12 09:14:06 +0000

    Add Site Key to Options

--- src/Pandorax.Recaptcha/RecaptchaOptions.cs
+++ src/Pandorax.Recaptcha/RecaptchaOptions.cs
@@ -4,6 +4,8 @@ namespace Pandorax.Recaptcha
 {
     public class RecaptchaOptions
     {
+        public string SiteKey { get; set; }
+
         public string SecretKey { get; set; }
 
         public Uri RecaptchaVerifyUrl { get; set; } = new Uri(RecaptchaConstants.DefaultRecaptchaVerifyUrl);
