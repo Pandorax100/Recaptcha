340a793499818c20f603885ea311c797c0033f73
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2020-02-12 09:17:34 +0000

    Add Method for IConfiguration

--- src/Pandorax.Recaptcha/ServiceCollectionExtensions.cs
+++ src/Pandorax.Recaptcha/ServiceCollectionExtensions.cs
@@ -1,10 +1,16 @@
 using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Pandorax.Recaptcha
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddRecaptcha(this IServiceCollection services, IConfiguration configurationSection)
+        {
+            return AddRecaptcha(services, options => configurationSection.Bind(options));
+        }
+
         public static IServiceCollection AddRecaptcha(this IServiceCollection services, Action<RecaptchaOptions> configureAction)
         {
             services.Configure(configureAction);
