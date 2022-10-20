870d7f70dcb25fd28625288583e9452b12d30e43
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2022-10-20 16:42:52 +0100

    Convert validation response to record

--- src/Pandorax.Recaptcha/ValidationResponse.cs
+++ src/Pandorax.Recaptcha/ValidationResponse.cs
@@ -7,7 +7,7 @@ namespace Pandorax.Recaptcha
     /// <summary>
     /// The reCAPTCHA validation response.
     /// </summary>
-    public class ValidationResponse
+    public record ValidationResponse
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ValidationResponse"/> class.
@@ -22,36 +22,36 @@ namespace Pandorax.Recaptcha
         /// Gets a value indicating whether this request was a valid reCAPTCHA token.
         /// </summary>
         [JsonPropertyName("success")]
-        public bool Success { get; internal set; }
+        public bool Success { get; init; }
 
         /// <summary>
         /// Gets the score for this request (0.0 - 1.0).
         /// </summary>
         [JsonPropertyName("score")]
-        public double Score { get; private set; }
+        public double Score { get; init; }
 
         /// <summary>
         /// Gets the action name for this request (important to verify).
         /// </summary>
         [JsonPropertyName("action")]
-        public string Action { get; private set; }
+        public string Action { get; init; }
 
         /// <summary>
         /// Gets timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ).
         /// </summary>
         [JsonPropertyName("challenge_ts")]
-        public DateTimeOffset ChallengeTimeStamp { get; private set; }
+        public DateTimeOffset ChallengeTimeStamp { get; init; }
 
         /// <summary>
         /// Gets the hostname of the site where the reCAPTCHA was solved.
         /// </summary>
         [JsonPropertyName("hostname")]
-        public string Hostname { get; private set; }
+        public string Hostname { get; init; }
 
         /// <summary>
         /// Gets the list of error codes returned from the validation request.
         /// </summary>
         [JsonPropertyName("error-codes")]
-        public List<string> ErrorCodes { get; private set; }
+        public List<string> ErrorCodes { get; init; }
     }
 }
